66fac59440d94dee174bda644d810574a568215a
Polina-mind <popova.v.polina@gmail.com>
2021-03-24 22:22:17 +0200

    create

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Profile from "./components/Profile";
+import user from "./user.json";
+// import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <Profile
+        avatar={user.avatar}
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Profile.js
@@ -0,0 +1,47 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Profile = ({ avatar, name, tag, location, followers, views, likes }) => {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img
+          src={avatar}
+          alt="Аватар пользователя"
+          className="avatar"
+          width="200"
+        />
+        <p className="name">{name}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  followers: PropTypes.number.isRequired,
+  views: PropTypes.number.isRequired,
+  likes: PropTypes.number.isRequired,
+};
+
+export default Profile;
