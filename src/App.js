80b2b0583cd8f79a28c23eb698ab1ed30da3c8a6
Polina-mind <popova.v.polina@gmail.com>
2021-03-25 16:39:44 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Profile from "./components/Profile";
 import user from "./user.json";
-// import './App.css';
+import "./App.css";
 
 const App = () => {
   return (
