5ff95992cef71bab769454ec6fb998291926afe3
Polina-mind <popova.v.polina@gmail.com>
2021-04-08 22:41:34 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Profile from "./components/Profile";
+import Statistics from "./components/Statistics";
 import user from "./user.json";
+import statisticalData from "./statistical-data.json";
 import "./App.css";
 
 const App = () => {
@@ -15,6 +17,8 @@ const App = () => {
         views={user.stats.views}
         likes={user.stats.likes}
       />
+      {/* <Statistics title="Upload stats" stats={statisticalData} />; */}
+      {/* <Statistics stats={statisticalData} />; */}
     </div>
   );
 };
---
+++ src/components/Statistics.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Statistics = (title, { stats }) => {
+  return (
+    <section className="statistics">
+      <h2 className="title">{title && title}</h2>
+
+      <ul className="stat-list">
+        {stats.map(({ id, label, percentage }) => (
+          <li className="item" key={id}>
+            <span className="label">{label}</span>
+            <span className="percentage">{percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.defaultProps = {
+  title: "Upload stats",
+};
+
+Statistics.protoTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+export default Statistics;
